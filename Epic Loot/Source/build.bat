494050ef905caec3b3e1c867be9c264083991bed
Surfpup <Surfpup00@gmail.com>
2013-04-11 20:01:48 -0400

    Batch script for compiling

---
+++ Epic Loot/Source/build.bat
@@ -0,0 +1,8 @@
+set builder_dir=C:\Program Files (x86)\Steam\steamapps\common\terraria
+set modpack_dir=C:\Users\Surfpup\Documents\My Games\Terraria\ModPacks
+
+cd "%builder_dir%"
+
+"Modpack Builder.exe" "%~dp0\" "Epic Loot" "%~dp0..\\"
+copy "%~dp0\..\*.obj" "%modpack_dir%"
+pause
